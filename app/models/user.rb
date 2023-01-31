154dea7eb6d6c5c01341c1ae5870f37b51a6f337
DelzironBraz <delziron.braz@hotmail.com>
2023-01-31 18:53:54 -0300

    style: add new views on menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  before_action :authenticate_user!
-  
-  user_signed_in?
-
-  current_user
-
-  user_session
-
-  root to: 'home#index'
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :contacts
   root 'home#index'
   get 'home/about'
+  devise_for :users
+  resources :contacts
 end
